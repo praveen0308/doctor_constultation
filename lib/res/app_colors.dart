03ae8bdebbc7d123e9f4a2ed5360621980ee411a
Praveen Yadav <praveen.yadav@sanklan.com>
2022-01-19 20:41:24 +0530

    asset initialized

---
+++ lib/res/app_colors.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primarySwatchColor = MaterialColor(0xFF3898CC, {
+    50: Color.fromRGBO(56, 152, 204, .1),
+    100: Color.fromRGBO(56, 152, 204, .2),
+    200: Color.fromRGBO(56, 152, 204, .3),
+    300: Color.fromRGBO(56, 152, 204, .4),
+    400: Color.fromRGBO(56, 152, 204, .5),
+    500: Color.fromRGBO(56, 152, 204, .6),
+    600: Color.fromRGBO(56, 152, 204, .7),
+    700: Color.fromRGBO(56, 152, 204, .8),
+    800: Color.fromRGBO(56, 152, 204, .9),
+    900: Color.fromRGBO(56, 152, 204, 1),
+  });
+
+  static const primaryLightest = Color(0XFFE0E8FA);
+  static const primaryLight = Color(0XFFE0E8FA);
+}
---
+++ lib/res/style_text.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyle {
+  static final headline1 = GoogleFonts.roboto(
+      textStyle: const TextStyle(fontSize: 80.0, fontWeight: FontWeight.w700));
+}
