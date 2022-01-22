5107f87b3e6fa3cdd4d72cb6ab721c39d06199b1
Praveen Yadav <praveen.yadav@sanklan.com>
2022-01-22 15:52:00 +0530

    status bar style done

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:doctor_consultation/res/image_path.dart';
 import 'package:doctor_consultation/patient/dashboard/dashboard.dart';
 import 'package:doctor_consultation/widgets/app_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: AppColors.primary,
+        statusBarIconBrightness: Brightness.dark));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
